96348dafef35b593b15254d35a0f5b31472439f4
Algebraaaa <jamonet026@gmail.com>
2025-10-23 19:35:32 +0800

    Add files via upload

---
+++ widget.cpp
@@ -0,0 +1,98 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_btnAlign_Left_clicked()
+{
+    ui->lineEdit->setAlignment(Qt::AlignLeft);
+}
+
+
+void Widget::on_btnAlign_Center_clicked()
+{
+    ui->lineEdit->setAlignment(Qt::AlignCenter);
+}
+
+
+void Widget::on_btnAlign_Right_clicked()
+{
+    ui->lineEdit->setAlignment(Qt::AlignRight);
+}
+
+
+void Widget::on_btnFont_Bold_clicked(bool checked)
+{
+    QFont font=ui->lineEdit->font();
+    font.setBold(checked);
+    ui->lineEdit->setFont(font);
+}
+
+
+void Widget::on_btnFont_Italic_clicked(bool checked)
+{
+    QFont font=ui->lineEdit->font();
+    font.setItalic(checked);
+    ui->lineEdit->setFont(font);
+}
+
+
+void Widget::on_btnFont_UnderLine_clicked(bool checked)
+{
+    QFont font=ui->lineEdit->font();
+    font.setUnderline(checked);
+    ui->lineEdit->setFont(font);
+}
+
+
+void Widget::on_checkBox_Readonly_clicked(bool checked)
+{
+    ui->lineEdit->setReadOnly(checked);
+}
+
+
+void Widget::on_checkBox_Enable_clicked(bool checked)
+{
+    ui->lineEdit->setEnabled(checked);
+}
+
+
+void Widget::on_checkBox_ClearButton_clicked(bool checked)
+{
+    ui->lineEdit->setClearButtonEnabled(checked);
+}
+
+
+void Widget::on_radioGreen_clicked()
+{
+    QPalette plet=ui->lineEdit->palette();
+    plet.setColor(QPalette::Text,Qt::green);
+    ui->lineEdit->setPalette(plet);
+}
+
+
+void Widget::on_radioRed_clicked()
+{
+    QPalette plet=ui->lineEdit->palette();
+    plet.setColor(QPalette::Text,Qt::red);
+    ui->lineEdit->setPalette(plet);
+}
+
+
+void Widget::on_radioBlue_clicked()
+{
+    QPalette plet=ui->lineEdit->palette();
+    plet.setColor(QPalette::Text,Qt::blue);
+    ui->lineEdit->setPalette(plet);
+}
+
